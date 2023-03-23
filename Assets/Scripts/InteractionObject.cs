eb1fcc5823615c3425ef9b74fac262656fe5ca19
John Wohler <johndoubleyou11@gmail.com>
2023-03-23 00:12:26 -0400

    Almost finished

--- Assets/Scripts/InteractionObject.cs
+++ Assets/Scripts/InteractionObject.cs
@@ -1,18 +1,67 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class InteractionObject : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public enum InteractableType
     {
-        
+        nothing,
+        info,
+        pickup,
+        dialogue
     }
 
-    // Update is called once per frame
-    void Update()
+    [Header("Type of interactable")]
+    public InteractableType interType;
+
+    [Header("Simple Info Message")]
+    public string InfoMessage;
+    private TMP_Text InfoText;
+
+    [Header("Dialogue Text")]
+    [TextArea]
+    public string[] sentences;
+
+    public void Start()
     {
-        
+        InfoText = GameObject.Find("InfoText").GetComponent<TMP_Text>();
+    }
+
+    public void DebugTest()
+    {
+        Debug.Log("This is a "+gameObject.name);
+    }
+
+    public void Nothing()
+    {
+        Debug.LogWarning("Object "+this.gameObject.name+" has no type set.");
+    }
+
+    public void Info()
+    {
+        //InfoText.text = InfoMessage;
+
+        StartCoroutine(ShowInfo(InfoMessage, 2.5f));
+    }
+
+    public void Pickup()
+    {
+        Debug.Log("You picked up " + this.gameObject.name);
+        this.gameObject.SetActive(false);
+    }
+
+    public void Dialogue()
+    {
+
+    }
+
+    IEnumerator ShowInfo(string message, float delay)
+    {
+        InfoText.text = message;
+        yield return new WaitForSeconds(delay);
+        InfoText.text = null;
     }
 }
--- Assets/Scripts/PlayerInteraction.cs
+++ Assets/Scripts/PlayerInteraction.cs
@@ -12,11 +12,52 @@ public class PlayerInteraction : MonoBehaviour
     public InteractionObject currentInterObjectsScript = null;
 
 
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.Space) && currentInterObject == true)
+        {
+            CheckInteraction();
+        }
+    }
+
+    private void CheckInteraction()
+    {
+        if (currentInterObjectsScript.interType == InteractionObject.InteractableType.nothing)
+        {
+            currentInterObjectsScript.Nothing();
+        }
+
+        else if (currentInterObjectsScript.interType == InteractionObject.InteractableType.info)
+        {
+            currentInterObjectsScript.Info();
+        }
+
+        else if (currentInterObjectsScript.interType == InteractionObject.InteractableType.pickup)
+        {
+            currentInterObjectsScript.Pickup();
+        }
+
+        else if (currentInterObjectsScript.interType == InteractionObject.InteractableType.dialogue)
+        {
+            currentInterObjectsScript.Dialogue();
+        }
+    }
+
     private void OnTriggerStay2D(Collider2D other)
     {
         if (other.CompareTag("InterObject") == true)
         {
             currentInterObject = other.gameObject;
+            currentInterObjectsScript = currentInterObject.GetComponent<InteractionObject>();
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("InterObject") == true)
+        {
+            currentInterObject = null;
+            currentInterObjectsScript = null;
         }
     }
 
